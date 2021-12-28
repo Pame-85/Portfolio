de80608270577124355f8d800e1afe79b1d97dd6
Pamela <pamelafumagalli85@gmail.com>
2021-12-27 22:10:25 -0300

    agregando-cambios

--- main.js
+++ main.js
@@ -1,46 +1,39 @@
 // VARIABLES
-const menuMobile = document.getElementById("icono-burger")
-const menuSeccion = document.getElementById("menu-mobile")
-const links = document.querySelectorAll("#menu-mobile li")
+const menuMobile = document.getElementById("icono-burger");
+const menuSeccion = document.getElementById("menu-mobile");
+const links = document.querySelectorAll("#menu-mobile li");
 
 // DESPLIEGA O CIERRA MENÚ
 menuMobile.onclick = () => {
-    menuSeccion.classList.toggle("menu-desplegado")
-}
+  menuSeccion.classList.toggle("menu-desplegado");
+};
 
 // CIERRA MENÚ AL IR A LINK
 for (let link of links) {
-    link.onclick = () => {
-        menuSeccion.classList.remove("menu-desplegado")
-    }
+  link.onclick = () => {
+    menuSeccion.classList.remove("menu-desplegado");
+  };
 }
 
 // FILTRO PROYECTOS
-const buttons = document.querySelectorAll(".menu-proyectos")
-const proyectos = document.querySelectorAll(".proyectos")
-const botonTodos = document.getElementById("boton-todos")
+const buttons = document.querySelectorAll(".menu-proyectos");
+const proyectos = document.querySelectorAll(".proyectos");
+const botonTodos = document.getElementById("boton-todos");
 
-
-   
 for (let button of buttons) {
-    
-    button.onclick = () => {
-        botonTodos.classList.remove("buttonClicked")
-        for (let proyecto of proyectos) {
-            const boton = button.dataset.name
-            const proyectoFiltrado = proyecto.dataset.name
-                            
-            if (boton === proyectoFiltrado) {
-                proyecto.classList.remove("hidden")
-            }
-            else if (boton === "todos") {
-                proyecto.classList.remove("hidden")
-            }
-            else {
-                proyecto.classList.add("hidden")
-        
-            }
-        }
+  button.onclick = () => {
+    botonTodos.classList.remove("buttonClicked");
+    for (let proyecto of proyectos) {
+      const boton = button.dataset.name;
+      const proyectoFiltrado = proyecto.dataset.name;
+
+      if (boton === proyectoFiltrado) {
+        proyecto.classList.remove("hidden");
+      } else if (boton === "todos") {
+        proyecto.classList.remove("hidden");
+      } else {
+        proyecto.classList.add("hidden");
+      }
     }
-    
+  };
 }
